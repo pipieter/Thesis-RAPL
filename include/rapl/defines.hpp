64a89c9cf0a769b8eda9462db6a42287b008578c
Pim Pieters <pim.pieters@ugent.be>
2025-02-09 16:41:01 +0100

    Add more event counters

--- include/rapl/defines.hpp
+++ include/rapl/defines.hpp
@@ -4,9 +4,16 @@
 
 // clang-format off
 #define RAPL_BENCHMARK_COUNTERS_EVENTS {                                                                               \
+        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES},                                                                \
+        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS},                                                              \
         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_REFERENCES},                                                          \
         {PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES},                                                              \
-        {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_TASK_CLOCK}                                                                 \
+        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_INSTRUCTIONS},                                                       \
+        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES},                                                             \
+        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_BUS_CYCLES},                                                                \
+        {PERF_TYPE_HARDWARE, PERF_COUNT_HW_REF_CPU_CYCLES},                                                            \
+        {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_TASK_CLOCK},                                                                \
+        {PERF_TYPE_SOFTWARE, PERF_COUNT_SW_PAGE_FAULTS}                                                                \
 }
 // clang-format on
 
